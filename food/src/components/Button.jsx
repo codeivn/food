9078f299de64a730cef1a177e5d60d01a64298e4
ivncode7 <118070672+ivncode7@users.noreply.github.com>
2023-01-03 20:42:19 +0100

    Update

--- food/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Button({ title }) {
-  return (
-    <button className='flex border border-white p-1 px-5 mx-2 rounded-xl text-white hover:bg-white hover:text-black'>{title}</button>
-  )
-}
-
-export default Button
--- food/src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Logo() {
-  return (
-    <h1 className='flex font-thin text-2xl text-white hover:text-3xl'>food.</h1>
-  )
-}
-
-export default Logo
