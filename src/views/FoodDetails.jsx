3107b49e9d00d31bf1d9fae0e58d696804eecd26
ivncode7 <118070672+ivncode7@users.noreply.github.com>
2023-01-04 17:53:05 +0100

    Data edit

--- src/views/FoodDetails.jsx
+++ src/views/FoodDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import Button2 from '../components/Button2';
 
 import Food from '../components/Food';
 
@@ -16,16 +17,22 @@ function FoodDetails() {
   const filteredItem = data.filter(element => currentItem.includes(element.id))
 
   return (
-    <div>
-        <h2>Food {id}</h2>
-        <div>
+    <div className='flex flex-col justify-center items-center pt-52 md:'>
+      <div>
         {filteredItem.map((item, index) => (
-            <div key={index}>
+            <div>
                 {/* <Food title={item.title} price={item.price} image={item.image} type={item.type}/> */}
-                <h1>{item.type}</h1>
+                <img src={item.image} className='h-72 w-96 rounded-t-xl' />
+                <h1 className='text-3xl text-black font-thin'>{item.title}</h1>
+                <p className='text-2xl text-grey font-thin'>{item.price} $</p>
+                <p className='text-sm font-thin py-5'>{item.description}</p>
             </div>
-          ))}
-        </div>
+          ))}</div>
+          <div className='flex flex-row'>
+            <Button2 title="Order now"/>
+            <Button2 title="More"/>
+          </div>
+          
     </div>
   )
 }
